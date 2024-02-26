d23bd72f2139e83413f0428c2ecf415f6f2fe430
Esther <esther191299@gmail.com>
2024-02-26 13:15:59 +0100

    ci: initial commit ci/cd pipeline

---
+++ src/main/java/com/example/xyzbank/controller/Controller.java
@@ -0,0 +1,13 @@
+package com.example.xyzbank.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/helloworld")
+    public String greeting() {
+        return "hello world";
+    }
+}
