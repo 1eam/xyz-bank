a3a82c03f7d096f15bb8703768204a45ddf85727
Esther <esther191299@gmail.com>
2024-02-26 09:37:05 +0100

    initial commit

---
+++ src/main/java/com/example/xyzbank/XyzBankApplication.java
@@ -0,0 +1,13 @@
+package com.example.xyzbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XyzBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XyzBankApplication.class, args);
+    }
+
+}
